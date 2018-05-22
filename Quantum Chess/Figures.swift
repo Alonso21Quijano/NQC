a46e12073aded8d9e2fe8cb4162a7250b9e485a9
Mikhail Chekanov <alonsokihana@gmal.com>
2018-05-23 01:01:03 +0300

    add castles(not sure if it quants good)

--- Quantum Chess/Figures.swift
+++ Quantum Chess/Figures.swift
@@ -70,6 +70,7 @@ class figures: SKSpriteNode
         var did_blocked: Bool = false //Show if at least on one of boards the figure was blocked
         var passed: [quant_board] = []
         var conflict: [quant_board] = []
+        var has_castle_conflict: Bool = false
         var has_conflict = false
         var king_eaten = false
         
@@ -77,6 +78,7 @@ class figures: SKSpriteNode
         
         if !((x + Int(dx) >= 0) && (y + Int(dy) >= 0) && (x + Int(dx) < 8) && (y + Int(dy) < 8))
         {return false}
+        
         if parent.showboard.figs[x + Int(dx)][y + Int(dy)] != nil && (parent.showboard.figs[x + Int(dx)][y + Int(dy)] as! figures).ID != self.ID
         {
             has_conflict = true
@@ -84,6 +86,11 @@ class figures: SKSpriteNode
             {
                 king_eaten = true
             }
+        }
+        
+        if abs(self.ID) == 16 && abs(dx) == 2 && dy == 0 //if we move king far maybe it's a castle
+        {
+            has_castle_conflict = true
         }
         
         for board in parent.boards //on all board make move if possible
@@ -94,26 +101,53 @@ class figures: SKSpriteNode
             }
             if board.board[x][y] != 0
             {
-                if corr_moves(dx: Int(dx), dy: Int(dy), board: board) == 1
+                if corr_moves(dx: Int(dx), dy: Int(dy), board: board) == 1 && (!has_castle_conflict || (parent.showboard.figs[dx > 0 ? 7: 0][y] as! figures).ID == g_col * (dx > 0 ?  10 : 9))
+                //I ❤️ crocodiles
                 {
                     if let del_ind = conflict.index(where: {$0 === board})
                     {
                         conflict.remove(at: del_ind) //Hope it is working!
                     }
                     did_moved = true
-                    onTap()
                     passed.append(board)
                     board.set(i: x,j: y,val: 0)
                     x += Int(dx)
                     y += Int(dy)
+                    if self.ID == g_col * 9
+                    {
+                        board.l_rook_move[g_col == 1 ? 1:0] = true
+                    }
+                    if self.ID == g_col * 10
+                    {
+                        board.r_rook_move[g_col == 1 ? 1:0] = true
+                    }
+                    if self.ID == g_col * 16
+                    {
+                        board.king_move[g_col == 1 ? 1 : 0] = true
+                    }
                     if king_eaten && board.board[x][y] == -g_col && board.win == 0
                     {
                         board.win = g_col
                     }
                     board.set(i: x, j: y, val: self.g_col)
+                    if has_castle_conflict
+                    {
+                        if dx > 0
+                        {
+                            board.set(i: x-1, j: y, val: self.g_col)
+                        }
+                        else
+                        {
+                            board.set(i: x+1, j: y, val: self.g_col)
+                        }
+                    }
                 }
                 else
                 {
+                    if has_castle_conflict && conflict.index(where: {$0 === board}) == nil
+                    {
+                        conflict.append(board)
+                    }
                     did_blocked = true
                 }
             }
@@ -127,6 +161,7 @@ class figures: SKSpriteNode
         }
         if did_moved
         {
+            onTap()
             if conflict.count != 0
             {
                 let conflict_solution = (Int(arc4random_uniform(UInt32(passed.count + conflict.count) + 1)) > passed.count) //True -- you loose (opponents figure saved)
@@ -144,6 +179,21 @@ class figures: SKSpriteNode
                         parent.boards = parent.boards.filter {$0 !== board}
                     }
                     self.run(SKAction.move(by: CGVector(dx: move_x,dy: move_y), duration: 0.1))
+                    if has_castle_conflict //if the move is castle
+                    {
+                        if dx == 2
+                        {
+                            (parent.showboard.figs[7][y] as! figures).run(SKAction.move(by: CGVector(dx: -2 * parent.CellSize,dy: 0), duration: 0.1))
+                            (parent.showboard.figs[7][y] as! figures).x -= 2
+                            parent.showboard.set(i: x, j: y, val: parent.showboard.figs[7][y])
+                        }
+                        else
+                        {
+                            (parent.showboard.figs[0][y] as! figures).run(SKAction.move(by: CGVector(dx: 3 * parent.CellSize,dy: 0), duration: 0.1))
+                            (parent.showboard.figs[0][y] as! figures).x += 3
+                            parent.showboard.set(i: x, j: y, val: parent.showboard.figs[0][y])
+                        }
+                    }
                     parent.showboard.set(i: x, j: y, val: self)
                 }
             }
@@ -151,38 +201,42 @@ class figures: SKSpriteNode
             {
                 self.run(SKAction.move(by: CGVector(dx: move_x,dy: move_y), duration: 0.1))
                 parent.showboard.set(i: x, j: y, val: self)
+                if has_castle_conflict //if the move is castle
+                {
+                    if dx == 2
+                    {
+                        (parent.showboard.figs[7][y] as! figures).run(SKAction.move(by: CGVector(dx: -2 * parent.CellSize,dy: 0), duration: 0.1))
+                        (parent.showboard.figs[7][y] as! figures).x -= 2
+                        parent.showboard.set(i: x-1, j: y, val: parent.showboard.figs[7][y])
+                    }
+                    else
+                    {
+                        (parent.showboard.figs[0][y] as! figures).run(SKAction.move(by: CGVector(dx: 3 * parent.CellSize,dy: 0), duration: 0.1))
+                        (parent.showboard.figs[0][y] as! figures).x += 3
+                        parent.showboard.set(i: x+1, j: y, val: parent.showboard.figs[0][y])
+                    }
+                }
             }
             if !did_blocked
             {
                 parent.showboard.set(i: old_x, j: old_y, val: nil)
+                if has_castle_conflict
+                {
+                    if dx == 2
+                    {
+                        parent.showboard.set(i: 7, j: y, val: nil)
+                    }
+                    else
+                    {
+                        parent.showboard.set(i: 0, j: y, val: nil)
+                    }
+                }
             }
             parent.showboard.update(boards: parent.boards)
             parent.showboard.draw(parent: parent)
         }
         return did_moved
     }
-    
-    
-    /*func EatKing(parent: Board)
-    {
-        for board in parent.boards
-        {
-            if !board.has_king
-            {
-                let numb_of_won_boards = (parent.boards.filter {$0 === board}).count //hope this thing return 0 or 1 only
-                parent.boards = parent.boards.filter {$0 !== board}
-                if g_col == 1
-                {
-                    parent.w_won_boards += numb_of_won_boards
-                }
-                else
-                {
-                    parent.b_won_boards += numb_of_won_boards
-                }
-            }
-        }
-    }*/
-    // Do not this stuff anymore
     
     func put(ParentNode: Board, position: int2, boards: [quant_board]){
         ParentNode.addChild(self)
@@ -246,6 +300,18 @@ class king: figures
     {
         if board.board[x + dx][y + dy] == g_col
         {return 0}
+        if (abs(dx) == 2)&&(dy == 0)&&(!board.king_move[g_col == 1 ? 1 : 0])&&(dx > 0 && !board.r_rook_move[g_col == 1 ? 1 : 0] || dx<0 && !board.l_rook_move[g_col == 1 ? 1 : 0]) //castle
+        {
+            let sign =  dx > 0 ? 1 : -1
+            var ch_x = x + sign
+            while ch_x != 0 &&  ch_x != 7
+            {
+                if board.board[ch_x][y] != 0
+                {return 0}
+                ch_x += sign
+            }
+            return 1
+        }
         if (abs(dx) <= 1) && (abs(dy) <= 1) && (abs(dx) + abs(dy) != 0)
         {
             return 1
